3847052af7f987a9063655c4cdd062c69bb5a5fd
Senseal <63717302+Senseal@users.noreply.github.com>
2026-02-10 19:55:21 +0000

    Update Main JS.js

--- Main JS.js
+++ Main JS.js
@@ -1,29 +1,30 @@
 //**Random Text Script**
     const splashTexts = [
-      "What the dog doin?",
-      "Now with Mobile surport!!",
-      "My wife left me.",
-      "Touch fluffy tail.",
-      "Y̸o̴u̵ ̷W̴i̶l̵l̸ ̴A̴t̸t̸o̵n̴e̸ ̸F̸o̴r̸ ̴Y̷o̸u̷r̸ ̵S̶i̴n̷s̵",
-      "I hate it when people ask me what star sign I am. I am a sign from god. Start running.",
-      "0x3a 0x44",
-      "Senseal@Github.com:~$ gnome-open Index.html",
-      "Is cloudflare down again?",
-      '\"Don\'t you fucking dare steal any flavour texts from me" -Mofu',
-      "Gaming",
-      "Cant code? Ask stack overflow.",
-      "Can I just get a hug please?",
-      "I don't care who the HMRC sends I will not pay taxes.",
-      "Did you know that Osama Bin Ladens computer was found to contain loads of pirated anime and games on it? ",
-      "🗿",
-      "Shawty had them apple bottom jeans (jeans) Boots with the jeans (jeans) The whole club was lookin' at her jeans (jeans)",
-      "What has the dog done...",
-      "Now tell me, who was in the showers at Ram Ranch?",
-      "The HMRC is looking for me.",
-      "Did you know that Osama Bin Laden liked K-on?",
-      "Saddam Hussein's hiding place is somewhere on this website.",
-      "Fumo Fumo ᗜˬᗜ",
-      "Did you know that the critically acclaimed MMORPG Final Fantasy XIV has a free trial, and includes the entirety of A Realm Reborn AND the award-winning Heavensward expansion up to level 60 with no restrictions on playtime? Sign up, and enjoy Eorzea today!",
+        "What the dog doin?",
+        "Now with Mobile surport!!",
+        "My wife left me.",
+        "Touch fluffy tail.",
+        "Y̸o̴u̵ ̷W̴i̶l̵l̸ ̴A̴t̸t̸o̵n̴e̸ ̸F̸o̴r̸ ̴Y̷o̸u̷r̸ ̵S̶i̴n̷s̵",
+        "I hate it when people ask me what star sign I am. I am a sign from god. Start running.",
+        "0x3a 0x44",
+        "Senseal@Github.com:~$ gnome-open Index.html",
+        "Is cloudflare down again?",
+        '\"Don\'t you fucking dare steal any flavour texts from me" -Mofu',
+        "Gaming",
+        "Cant code? Ask stack overflow.",
+        "Can I just get a hug please?",
+        "I don't care who the HMRC sends I will not pay taxes.",
+       "Did you know that Osama Bin Ladens computer was found to contain loads of pirated anime and games on it? ",
+        "🗿",
+        "Shawty had them apple bottom jeans (jeans) Boots with the jeans (jeans) The whole club was lookin' at her jeans (jeans)",
+        "What has the dog done...",
+        "Now tell me, who was in the showers at Ram Ranch?",
+        "The HMRC is looking for me.",
+        "Did you know that Osama Bin Laden liked K-on?",
+        "Saddam Hussein's hiding place is somewhere on this website.",
+        "Fumo Fumo ᗜˬᗜ",
+        "Now with supprot for 3ds",
+        "Did you know that the critically acclaimed MMORPG Final Fantasy XIV has a free trial, and includes the entirety of A Realm Reborn AND the award-winning Heavensward expansion up to level 60 with no restrictions on playtime? Sign up, and enjoy Eorzea today!",
     ];
     var randomNumber = Math.floor(Math.random() * splashTexts.length); // Pick a random number
     var textElement = document.getElementById("randomText"); // Get the element by ID and swap the text
@@ -34,4 +35,5 @@
 console.log("Oh look Creddits, My Favourite.");
 console.log("JS used in sites old format (window colsing) https://twitter.com/thetimgor");
 console.log("'Shit. uh. I dunno.' - Sophie (Splash texts maker)");
+
 
